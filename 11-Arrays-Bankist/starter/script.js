5ec6c7574530b57eed415953b37084dde02dab8e
BachNVMume <bachnv@mumesoft.com>
2025-03-13 08:03:02 +0700

    update code 13/3

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -211,19 +211,102 @@ GOOD LUCK 😀
 //   .reduce((acc, cur) => acc + cur, 0);
 // console.log(totalDepositsUSD);
 
-// find method
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// // find method
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
-//findLast and findLastIndex Methods
-const lastWithdrawl = movements.findLast(mov => mov < 0);
-// EQUALITY
-console.log(movements.includes(-130));
-// SOME: CONDITION
-const anyDeposit = movements.some(mov => mov > 0);
-console.log(anyDeposit);
-// EVERY
+// //findLast and findLastIndex Methods
+// const lastWithdrawl = movements.findLast(mov => mov < 0);
+// // EQUALITY
+// console.log(movements.includes(-130));
+// // SOME: CONDITION
+// const anyDeposit = movements.some(mov => mov > 0);
+// console.log(anyDeposit);
+// // EVERY
 
-console.log(movements.every(mov => mov > 0));
+// console.log(movements.every(mov => mov > 0));
+
+// // Flat
+// const arrFlat = [[1, 2, 3], [4, 5, 6], 7, 8];
+
+// console.log(arrFlat.flat());
+
+// const arrDeep = [[[1, 2], 3], [4, [5, 6]], 7, 8];
+// console.log(arrDeep.flat(2));
+
+/*
+This time, Julia and Kate are studying the activity levels of different dog breeds.
+
+YOUR TASKS:
+1. Store the the average weight of a "Husky" in a variable "huskyWeight"
+2. Find the name of the only breed that likes both "running" and "fetch" ("dogBothActivities" variable)
+3. Create an array "allActivities" of all the activities of all the dog breeds
+4. Create an array "uniqueActivities" that contains only the unique activities (no activity repetitions). HINT: Use a technique with a special data structure that we studied a few sections ago.
+5. Many dog breeds like to swim. What other activities do these dogs like? Store all the OTHER activities these breeds like to do, in a unique array called "swimmingAdjacent".
+6. Do all the breeds have an average weight of 10kg or more? Log to the console whether "true" or "false".
+7. Are there any breeds that are "active"? "Active" means that the dog has 3 or more activities. Log to the console whether "true" or "false".
+
+BONUS: What's the average weight of the heaviest breed that likes to fetch? HINT: Use the "Math.max" method along with the ... operator.
+
+TEST DATA:
+*/
+
+const breeds = [
+  {
+    breed: 'German Shepherd',
+    averageWeight: 32,
+    activities: ['fetch', 'swimming'],
+  },
+  {
+    breed: 'Dalmatian',
+    averageWeight: 24,
+    activities: ['running', 'fetch', 'agility'],
+  },
+  {
+    breed: 'Labrador',
+    averageWeight: 28,
+    activities: ['swimming', 'fetch'],
+  },
+  {
+    breed: 'Beagle',
+    averageWeight: 12,
+    activities: ['digging', 'fetch'],
+  },
+  {
+    breed: 'Husky',
+    averageWeight: 26,
+    activities: ['running', 'agility', 'swimming'],
+  },
+  {
+    breed: 'Bulldog',
+    averageWeight: 36,
+    activities: ['sleeping'],
+  },
+  {
+    breed: 'Poodle',
+    averageWeight: 18,
+    activities: ['agility', 'fetch'],
+  },
+];
+//1
+const huskyWeight = breeds.find(acc => acc.breed === 'Husky').averageWeight;
+console.log(huskyWeight);
+
+//2
+const breedName = breeds.find(
+  acc => acc.activities.includes('running') && acc.activities.includes('fetch')
+).breed;
+console.log(breedName);
+
+//3
+const allActivities = breeds.map(acc => acc.activities).flat();
+console.log(allActivities);
+
+//4
+const uniqueActivities = new Set(allActivities);
+console.log(uniqueActivities);
+
+//5
+const swimmingAdjacent = uniqueActivities.
 
 // Data
 const account1 = {
@@ -255,6 +338,19 @@ const account4 = {
 };
 
 const accounts = [account1, account2, account3, account4];
+
+// const overalBalance = accounts
+//   .map(acc => acc.movements)
+//   .flat()
+//   .reduce((acc, mov) => acc + mov, 0);
+// console.log(overalBalance);
+
+// //flatMap
+
+// const overalBalance2 = accounts
+//   .flatMap(acc => acc.movements)
+//   .reduce((acc, mov) => acc + mov, 0);
+// console.log(overalBalance2);
 
 // Elements
 const labelWelcome = document.querySelector('.welcome');
